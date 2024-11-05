98ee3a52bce80c2d36167b659cc435f44a9666ff
Thinh Le <155042515+nerdqueen92@users.noreply.github.com>
2024-11-05 21:12:19 +0700

    Update kagglehub_api.py

--- kagglehub_api.py
+++ kagglehub_api.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/download_kaggle_dataset', methods=['GET'])
 def download_kaggle_dataset():
+    print('Start Downloading')
     # Download dataset using kagglehub
     path = kagglehub.dataset_download("mmontiel/tiny-lisa-traffic-sign-detection-dataset")
     print("Path to dataset files:", path)
@@ -14,6 +15,7 @@ def download_kaggle_dataset():
     # Move dataset to Google Drive if needed, otherwise keep on server
     destination_directory = "/content"  # Change this to your desired location
     shutil.move(path, destination_directory)
+    print('Done Downloading')
     return jsonify({"message": f"Dataset moved to {destination_directory}"}), 200
 
 if __name__ == '__main__':
